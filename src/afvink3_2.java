11e73e022d79f02f1435da2899d003ab6cb76a5e
daria <do.gajewska@student.han.nl>
2022-12-01 13:27:54 +0100

    code to open a file and make a chr-gene map

--- src/afvink3_2.java
+++ src/afvink3_2.java
@@ -1,29 +1,51 @@
-import javax.swing.*;
 import java.io.*;
+import java.util.*;
 
 public class afvink3_2 {
-    private static String fileName;
     private static BufferedReader inFile;
-
+    private static ArrayList<String> geneArr;
+    private static Map<String, ArrayList> map = new HashMap<>();
     public static void main(String[] args) {
-        afvink3_2 af = new afvink3_2();
-        openFile();
+        afvink3_2 chrList = new afvink3_2();
+        nestedList();
     }
-    public static void openFile() {
-        // to open a file
+
+    /**
+     * Open a file with info about homo_sapiens genes
+     * Add genes of a chromosome in a list that is nested in a list of chromosomes
+     */
+    public static void nestedList() {
         try {
             String line;
-            inFile = new BufferedReader(new FileReader("C:\\Users\\daria\\OneDrive\\Bureaublad\\Homo_sapiens.txt"));
+            // to open a file
+            inFile = new BufferedReader(new FileReader(
+                    "C:\\Users\\daria\\OneDrive\\Bureaublad\\Homo_sapiens.txt"));
+            // loop through the lines of a file
             while ((line=inFile.readLine())!=null){
-                System.out.println(line+"\n");
+                String chr = line.split("\t")[6];
+                String gene = line.split("\t")[2];
+                // check if the chromosome is already used
+                if(map.containsKey(chr)){
+                    // the value of a map (the array list of genes) will be grabbed and edited by adding a new gene
+                    geneArr = map.get(chr);
+                    geneArr.add(gene);
+                } else {
+                    // if the chromosome is not used yet a new gene list will be created
+                    // and the new key+value will be added to a map
+                    geneArr = new ArrayList<>();
+                    geneArr.add(gene);
+                    map.put(chr, geneArr);
+                }
             }
-        }
-        catch (FileNotFoundException fnfe) {
+
+//            System.out.println(map.get("12"));
+
+        } catch (FileNotFoundException fnfe) {
             System.out.println("File not found");
         } catch (IOException ioe) {
             System.out.println("File can not be read");
         } catch (Exception e) {
-            System.out.println("Undefined error");
+            System.out.println("Undefined error: "+e);
         }
     }
 }
